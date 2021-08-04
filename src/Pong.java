93394462b2e765f1472b7c722e23a7e6dc1d6eb4
Elensarr <nkhalaman@gmail.com>
2021-08-04 22:18:17 +1200

    Pong and PongPanel classes created

--- src/Pong.java
+++ src/Pong.java
@@ -13,23 +13,32 @@ import javax.swing.JFrame;
 public class Pong extends JFrame {
 	
 	//============== CONSTANTS =========================
-	public static final String WINDOW_TITLE = "Pong";
-	public static final int WINDOW_WIDTH = 800;
-	public static final int WINDOW_HEIGHT = 600;
-
-	public Pong() {
-		setTitle(WINDOW_TITLE);
-		setSize(WINDOW_WIDTH,WINDOW_HEIGHT);
-		setResizable(false);
-		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		new Pong();
+		public static final String WINDOW_TITLE = "Pong";		
+		public static final int WINDOW_WIDTH = 800;
+		public static final int WINDOW_HEIGHT = 600;
 		
 
-	}
+		public Pong() {
+			setTitle(WINDOW_TITLE);
+			setSize(WINDOW_WIDTH,WINDOW_HEIGHT);
+			setResizable(false);
+			
+			// adds panel to the frame
+			add(new PongPanel());
+			setVisible(true);
+			setDefaultCloseOperation(EXIT_ON_CLOSE);
+		}
+		
+		public static void main(String[] args) {
+			
+			//delays the GUI creation task until until the initial thread's tasks have been completed
+			// can be called from any thread
+			javax.swing.SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
+					new Pong();
+				}
+			}); // closes Runnable
+				
+		}// closes main method
 
-}
+} // closes Pong class
