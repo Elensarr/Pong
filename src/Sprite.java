1137e25d37fda9f139b451594d211aea2cf4db7c
Elensarr <nkhalaman@gmail.com>
2021-08-06 22:23:29 +1200

    getters, setters, rectangle in Sprite class

--- src/Sprite.java
+++ src/Sprite.java
@@ -1,10 +1,12 @@
 import java.awt.Color;
+import java.awt.Rectangle;
 
 public class Sprite {
 
 	//variables
 	private int xPosition, yPosition, xVelocity, yVelocity, width, height;
 	private Color colour;
+	private int initial_xPosition, initial_yPosition;
 	
 	
 	//constructor
@@ -13,12 +15,50 @@ public class Sprite {
 	}
 	
 	//=== SETTERS METHODS ====
+	
 	public void set_xPosition(int new_xPosition) {
 		this.xPosition = new_xPosition;
 	}
 	public void set_yPosition(int new_yPosition) {
 		this.yPosition = new_yPosition;
 	}
+	
+	public void set_initial_Position(int initialX, int initialY) {
+		initial_xPosition = initialX;
+		initial_yPosition = initialY;
+	}
+			
+	public void set_xPosition(int new_xPosition, int panelWidth) {
+		
+		this.xPosition = new_xPosition;
+		
+		// check if new position is inside the panel		
+		if ((xPosition + get_width()) > panelWidth) {
+			this.xPosition = panelWidth - get_width();
+		}
+		else if (xPosition < 0) {
+			this.xPosition = 0;
+		}
+	}
+	
+	public void set_yPosition(int new_yPosition, int panelHeight) {
+		this.yPosition = new_yPosition;
+		
+		// check if new position is inside the panel		
+		if ((yPosition + get_height()) > panelHeight) {
+			this.yPosition = panelHeight - get_height();
+		}
+		else if (yPosition < 0) {
+			this.yPosition = 0;
+		}
+	}
+	
+	// set the x and y position of the object to the initial x and y positions.
+			private void resetToInitialPosition() {
+				set_xPosition(initial_xPosition);
+				set_yPosition(initial_yPosition);			
+			}
+			
 	public void set_xVelocity(int new_xVelocity) {
 		this.xVelocity = new_xVelocity;
 	}
@@ -57,5 +97,12 @@ public class Sprite {
 	public Color get_color() {
 		return colour;
 	}
+	
+	// ===========================
+	
+	// create rectangle object
+	public Rectangle getRectangle() {
+        return new Rectangle(get_xPosition(), get_yPosition(), get_width(), get_height());
+    }	
 	
 }
